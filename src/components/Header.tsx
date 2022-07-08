4dfbaf3c8224ac13e5e61178a20e0e1d1856ae88
Patrick Thomaz <plthomaz@gmail.com>
2022-07-08 12:44:18 -0300

    [Implement]: User in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -46,6 +46,35 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" /> 
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Patrick Thomaz</Text>
+            <Text color="gray.300" fontSize="small">
+              plthomaz@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Patrick Thomaz" src="http://github.com/patrickt26.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
